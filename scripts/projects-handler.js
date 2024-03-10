f08e4f382990f6c16083aa19004fb20f2b87e034
nour <nourelturky9@gmail.com>
2024-03-10 16:09:38 +0200

    portfolio  using js

---
+++ scripts/projects-handler.js
@@ -0,0 +1,30 @@
+var projectsDiv = document.getElementById("projects");
+for (let i = 0; i < PROJECTS.length; i++) {
+  var currentProject = PROJECTS[i];
+  //div
+  var projectDiv = document.createElement("div");
+  var divStyle = document.createAttribute("style");
+  divStyle.value = "display:inline-block; width:300px; margin:10px";
+  projectDiv.setAttributeNode(divStyle);
+  //h3
+  var title = document.createElement("h3");
+  var titleNode = document.createTextNode(currentProject.title);
+  title.appendChild(titleNode);
+  //img
+  var image = document.createElement("img");
+  var source = document.createAttribute("src");
+  source.value = "./images/" + currentProject.img;
+  image.setAttributeNode(source);
+  var imgStyle = document.createAttribute("style");
+  imgStyle.value = "width:200px; height:200px";
+  image.setAttributeNode(imgStyle);
+  //p
+  var p = document.createElement("p");
+  var pNode = document.createTextNode(currentProject.description);
+  p.appendChild(pNode)
+
+  projectDiv.appendChild(title)
+  projectDiv.appendChild(image)
+  projectDiv.appendChild(p)
+  projectsDiv.appendChild(projectDiv)
+}
---
+++ scripts/projects.js
@@ -0,0 +1,19 @@
+var PROJECTS=[
+    {
+        img:"project1.png",
+        title:"project1",
+        description:"about project1"
+    },
+    {
+        img:"project2.png",
+        title:"project2",
+        description:"about project2"
+    },
+    {
+        img:"project3.png",
+        title:"project3",
+        description:"about project3"
+    },
+
+
+]
